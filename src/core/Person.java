258db42767ca3dab8f50d68029c7816a6d064542
Enrique Jesus Peinado Torres <ejpeinado@uninorte.edu.co>
2025-03-20 08:21:54 -0500

    added person class

---
+++ src/core/Person.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author ejpeinado
+ */
+public class Person {
+    private int id;
+    private String  name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", name=" + name + ", age=" + age + '}';
+    }
+    
+    
+}
--- src/main/main.java
+++ src/main/main.java
@@ -4,6 +4,8 @@
  */
 package main;
 
+import core.Person;
+
 /**
  *
  * @author ejpeinado
@@ -12,6 +14,8 @@ public class main {
     
     public static void main(String[] args) {
         System.out.println("hallo");
+        Person persona = new Person(1000, "Pedro", 14);
+        System.out.println(persona);
     }
     
 }
